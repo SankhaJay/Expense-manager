5642232a057ad7ac84d8eb1d84af164666b4886b
sankhajay <sankha.jayalath@madmobile.com>
2022-10-30 07:49:04 +0530

    add new expense button and cancel button functionality

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
 
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isFormVisible, setFormVisible] = useState(false);
+
   const saveExpenseDataHandler = (expenseDataEntered) => {
     const expenseData = {
       ...expenseDataEntered,
@@ -13,9 +15,24 @@ const NewExpense = (props) => {
 
     props.onAddExpenseData(expenseData);
   };
+
+  const addExpenseButtonHandler = () => {
+    setFormVisible((prevState) => {
+      return !prevState;
+    });
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isFormVisible && (
+        <button onClick={addExpenseButtonHandler}>Add New Expense</button>
+      )}
+      {isFormVisible && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={addExpenseButtonHandler}
+        />
+      )}
     </div>
   );
 };
